a68464e1e788a7912b0050d8dcd3b88aa13b0af9
alexandersusikov <susikov.alexander@gmail.com>
2013-01-08 23:06:06 +0400

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dancewear::Application.config.secret_token = 'b0b4651645e5da8e3cfd193eb7932e8ab7e02617a4c275b836a8548650fb8667207f200c5b27c28956ac574cb01e73f11c31d0747dd80b4df05bfbcefbcc2bbe'
